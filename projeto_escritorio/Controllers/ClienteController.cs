4a209c88462d3552b64607ba42d357d0aacdccb0
Mateus Romani <mateusromani@gmail.com>
2024-01-22 14:47:29 -0300

    criando o controlador da classe Cliente

---
+++ projeto_escritorio/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projeto_escritorio.Models;
+using System.Runtime.CompilerServices;
+
+namespace projeto_escritorio.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ClienteController: ControllerBase
+{
+    private static List<Cliente> clientes = new List<Cliente>();
+
+    [HttpPost]
+    public void AdicionaCliente([FromBody]Cliente cliente)
+    {
+        clientes.Add(cliente);
+        Console.WriteLine(cliente.Nome);
+        Console.WriteLine(cliente.Endereco);
+    }
+}
