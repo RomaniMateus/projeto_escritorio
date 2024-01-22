d0ddb8833b5d9a91209a5197baf418040a4d9e52
Mateus Romani <mateusromani@gmail.com>
2024-01-22 14:47:10 -0300

    criando a classe Cliente

---
+++ projeto_escritorio/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_escritorio.Models;
+
+public class Cliente
+{
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Endereco { get; set; }
+    public string Telefone { get; set; }
+    
+    //Adicionar o(s) ID(s) da(s) loja(s) 
+}
+
