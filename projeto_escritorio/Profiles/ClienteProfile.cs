48ec97595d4a30c00e429e7983da52b3a3805f3f
Mateus Romani <mateusromani@gmail.com>
2024-01-25 18:23:32 -0300

    Criando o perfil do AutoMapper

---
+++ projeto_escritorio/Profiles/ClienteProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using projeto_escritorio.Data.Dtos;
+using projeto_escritorio.Models;
+
+namespace projeto_escritorio.Profiles;
+
+public class ClienteProfile : Profile
+{
+    public ClienteProfile()
+    {
+        CreateMap<CreateClienteDto, Cliente>();
+    }
+}
