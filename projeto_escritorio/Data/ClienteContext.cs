41f3ccce4c8e3f687d912edb0d093f1c2d636bc1
Mateus Romani <mateusromani@gmail.com>
2024-01-25 18:26:25 -0300

    alterações visuais no código

--- projeto_escritorio/Data/ClienteContext.cs
+++ projeto_escritorio/Data/ClienteContext.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using projeto_escritorio.Models;
 
-namespace projeto_escritorio.Data;
-
-public class ClienteContext : DbContext
+namespace projeto_escritorio.Data
 {
-    public ClienteContext(DbContextOptions<ClienteContext>opts) : base(opts)
+    public class ClienteContext : DbContext
     {
+        public ClienteContext(DbContextOptions<ClienteContext>opts) : base(opts)
+        {
         
-    }
+        }
 
-    public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
 }
--- projeto_escritorio/Migrations/ClienteContextModelSnapshot.cs
+++ projeto_escritorio/Migrations/ClienteContextModelSnapshot.cs
@@ -6,45 +6,44 @@ using projeto_escritorio.Data;
 
 #nullable disable
 
-namespace projeto_escritorio.Migrations
+namespace projeto_escritorio.Migrations;
+
+[DbContext(typeof(ClienteContext))]
+partial class ClienteContextModelSnapshot : ModelSnapshot
 {
-    [DbContext(typeof(ClienteContext))]
-    partial class ClienteContextModelSnapshot : ModelSnapshot
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.10")
-                .HasAnnotation("Relational:MaxIdentifierLength", 64);
+        modelBuilder
+            .HasAnnotation("ProductVersion", "6.0.10")
+            .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-            modelBuilder.Entity("projeto_escritorio.Models.Cliente", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+        modelBuilder.Entity("projeto_escritorio.Models.Cliente", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    b.Property<string>("Cpf")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Cpf")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("Endereco")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Endereco")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("Nome")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Nome")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("Telefone")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Telefone")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Clientes");
-                });
+                b.ToTable("Clientes");
+            });
 #pragma warning restore 612, 618
-        }
     }
 }
