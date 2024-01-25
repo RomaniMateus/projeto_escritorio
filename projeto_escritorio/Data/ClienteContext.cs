86c7decaf23769417386db10fbb0c56b3af493ea
Mateus Romani <mateusromani@gmail.com>
2024-01-25 16:24:26 -0300

    criando a conexão com o banco de dados

---
+++ projeto_escritorio/Data/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projeto_escritorio.Models;
+
+namespace projeto_escritorio.Data;
+
+public class ClienteContext : DbContext
+{
+    public ClienteContext(DbContextOptions<ClienteContext>opts) : base(opts)
+    {
+        
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+}
--- projeto_escritorio/Program.cs
+++ projeto_escritorio/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using projeto_escritorio.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ClienteConnection");
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ClienteContext>(opts => opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
