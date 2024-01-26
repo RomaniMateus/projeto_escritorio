f74e651b8ab0dfaaeefadd6cb6a8038f14badaa9
Mateus Romani <mateusromani@gmail.com>
2024-01-26 17:50:26 -0300

    Criando os DTO's

---
+++ projeto_escritorio/Data/Dtos/CreateLojaDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_escritorio.Data.Dtos;
+
+public class CreateLojaDto
+{
+    [Required(ErrorMessage = "O campo 'nome' é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O campo 'CNPJ' é obrigatório")]
+    [StringLength(18, ErrorMessage = "O campo 'CNPJ' deve possuir 18 caracteres")]
+    public string Cnpj { get; set; }
+
+    [Required(ErrorMessage = "O campo 'Endereço' é obrigatório")]
+    [MinLength(20, ErrorMessage = "O campo 'Endereço' deve possui ao menos 20 caracteres")]
+    public string Endereco { get; set; }
+}
---
+++ projeto_escritorio/Data/Dtos/ReadLojaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_escritorio.Data.Dtos;
+
+public class ReadLojaDto
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Cnpj { get; set; }
+
+    public string Endereco { get; set; }
+}
