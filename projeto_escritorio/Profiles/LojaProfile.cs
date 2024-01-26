6eb1f5be84fd83ea77881087a55426b1810def85
Mateus Romani <mateusromani@gmail.com>
2024-01-26 17:49:17 -0300

    Criando perfil para a entidade Loja

---
+++ projeto_escritorio/Profiles/LojaProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using projeto_escritorio.Data.Dtos;
+using projeto_escritorio.Models;
+
+namespace projeto_escritorio.Profiles;
+
+public class LojaProfile : Profile
+{
+    public LojaProfile()
+    {
+        CreateMap<CreateLojaDto, Loja>();
+        CreateMap<Loja, ReadLojaDto>();
+        CreateMap<UpdateLojaDto, Loja>();
+    }
+}
