2e2e1d200e0b8b42272178157a757c29be2eabb4
Mateus Romani <mateusromani@gmail.com>
2024-01-26 11:27:20 -0300

    Criando um DTO para o método PUT

---
+++ projeto_escritorio/Data/Dtos/UpdateClienteDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_escritorio.Data.Dtos;
+
+public class UpdateClienteDto
+{
+
+    [Required(ErrorMessage = "O nome do cliente é obrigatório")]
+    [MinLength(10, ErrorMessage = "O nome deve possuir pelo menos 10 caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O CPF do cliente é obrigatório")]
+    [MinLength(14, ErrorMessage = "O CPF não deve possuir mais de 14 caracteres")]
+    public string Cpf { get; set; }
+
+    [Required(ErrorMessage = "O endereço do cliente é obrigatório")]
+    [MinLength(20, ErrorMessage = "O endereço deve possuir pelo menos 20 caracteres")]
+    public string Endereco { get; set; }
+
+    [Required(ErrorMessage = "O telefone do cliente é obrigatório")]
+    [MinLength(10, ErrorMessage = "O telefone deve possuir pelo menos 10 caracteres")]
+    public string Telefone { get; set; }
+
+    //Adicionar o(s) ID(s) da(s) loja(s) 
+}
