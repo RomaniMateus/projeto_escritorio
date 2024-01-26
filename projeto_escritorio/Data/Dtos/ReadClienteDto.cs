5a6bd76b8e7d855f8f23b818b5e03251f4532b48
Mateus Romani <mateusromani@gmail.com>
2024-01-26 12:07:10 -0300

    criando DTO de leitura

---
+++ projeto_escritorio/Data/Dtos/ReadClienteDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_escritorio.Data.Dtos
+{
+    public class ReadClienteDto
+    {
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Telefone { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+
+        //Adicionar o(s) ID(s) da(s) loja(s) 
+    }
+}
